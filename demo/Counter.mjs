f8aac1420f34a52e51f8764f73fba834380bde6b
Ayo <ramon.aycojr@gmail.com>
2023-11-20 22:14:37 +0100

    counter example

--- demo/Counter.mjs
+++ demo/Counter.mjs
@@ -3,12 +3,10 @@ import WebComponent from "../src/WebComponent.js";
 
 export class Counter extends WebComponent {
   static properties = ["count"];
-
   onInit() {
     this.props.count = 0;
     this.onclick = () => ++this.props.count;
   }
-
   get template() {
     return `<button id="btn">${this.props.count}</button>`;
   }

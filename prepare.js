31cd51549801f81fe1ff548bab05f9c1a83d260b
Ayo <ramon.aycojr@gmail.com>
2023-11-21 13:32:10 +0100

    don't ship devDependencies

--- prepare.js
+++ prepare.js
@@ -3,5 +3,5 @@ import { createRequire } from "node:module";
 
 const require = createRequire(import.meta.url);
 const pkg = require("./package.json");
-const { scripts, ...rest } = pkg;
+const { scripts, devDependencies, ...rest } = pkg;
 writeFileSync("./dist/package.json", JSON.stringify(rest));

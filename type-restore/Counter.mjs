3e8334f2c198759c61238e1ccf2fae3cdadbde71
Ayo <ramon.aycojr@gmail.com>
2023-11-26 19:50:29 +0100

    feat(site): update content

--- type-restore/Counter.mjs
+++ type-restore/Counter.mjs
@@ -4,8 +4,12 @@ import WebComponent from "../src/WebComponent.js";
 export class Counter extends WebComponent {
   static properties = ["count"];
   onInit() {
-    this.props.count = 0;
+    this.props.count = 1;
     this.onclick = ()=> ++this.props.count
+    let double = () => this.props.count * 2;
+    console.log(double());
+    this.props.count = 3;
+    console.log(double());
   }
   get template() {
     return `<button>${this.props.count}</button>`;

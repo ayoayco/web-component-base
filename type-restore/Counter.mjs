6027f664b38af2443bd6e8928bcb678c00cbd370
Ayo <ramon.aycojr@gmail.com>
2023-11-26 03:43:41 +0100

    feat: preserve props type

---
+++ type-restore/Counter.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import WebComponent from "../src/WebComponent.js";
+
+export class Counter extends WebComponent {
+  static properties = ["count"];
+  onInit() {
+    this.props.count = 0;
+    this.onclick = ()=> ++this.props.count
+  }
+  get template() {
+    return `<button>${this.props.count}</button>`;
+  }
+}
+
+customElements.define("my-counter", Counter);

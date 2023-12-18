64fd600435cebc21978004ea892650d42832638f
Ayo Ayco <ramon.aycojr@gmail.com>
2023-12-18 23:12:18 +0100

    chore: publish dist from root dir (#32)

--- prepare.js
+++
@@ -1,7 +0,0 @@
-import { writeFileSync } from "node:fs";
-import { createRequire } from "node:module";
-
-const require = createRequire(import.meta.url);
-const pkg = require("./package.json");
-const { scripts, devDependencies, ...rest } = pkg;
-writeFileSync("./dist/package.json", JSON.stringify(rest));

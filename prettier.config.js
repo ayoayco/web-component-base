7fcbbfb28daefba3cefcfb7e869ab7edc40f6e06
Ayo Ayco <ayo@ayco.io>
2024-12-19 22:41:46 +0100

    chore: set up prettier

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+}
+
+export default config

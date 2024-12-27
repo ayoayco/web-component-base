18c7eaa58d9fa15ba6ce6e9c966ad534931ed2b7
Ayo Ayco <ayo@ayco.io>
2024-12-27 21:53:26 +0100

    chore: test precommit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,8 @@ import { includeIgnoreFile } from '@eslint/compat'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 
+const unused = 'hey'
+
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 const gitignorePath = path.resolve(__dirname, '.gitignore')

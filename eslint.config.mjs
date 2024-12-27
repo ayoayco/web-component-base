c0269c46699ce5a1972519957ba25626285ec83d
Ayo Ayco <ayo@ayco.io>
2024-12-27 16:26:39 +0100

    chore: reuse gitignore patterns for eslint ignores

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,21 @@
 import globals from 'globals'
 import pluginJs from '@eslint/js'
 import jsdoc from 'eslint-plugin-jsdoc'
+import { includeIgnoreFile } from '@eslint/compat'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const gitignorePath = path.resolve(__dirname, '.gitignore')
+
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   jsdoc.configs['flat/recommended'],
+  includeIgnoreFile(gitignorePath),
   {
     rules: {
       'no-unused-vars': 'warn',

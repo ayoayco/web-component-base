d10a164998a53e460614348b2b6df9379fe42ffc
Ayo Ayco <ayo@ayco.io>
2024-12-27 15:45:07 +0100

    chore: add eslint-plugin-jsdoc

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,12 @@
 import globals from 'globals'
 import pluginJs from '@eslint/js'
+import jsdoc from 'eslint-plugin-jsdoc'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
+  jsdoc.configs['flat/recommended'],
   {
     rules: {
       'no-unused-vars': 'warn',
